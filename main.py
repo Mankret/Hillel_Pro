e3145d4fa455200cd74e0e2a487f47d32c548903
Mankret <asilvergrave@gmail.com>
2023-01-01 17:30:54 +0200

    Updated parse_cookie function + tests added

--- main.py
+++ main.py
@@ -1,3 +1,6 @@
+
+from http.cookies import SimpleCookie
+
 def parse(query: str) -> dict:
     return {}
 
@@ -11,7 +14,10 @@ if __name__ == '__main__':
 
 
 def parse_cookie(query: str) -> dict:
-    return {}
+    cookie = SimpleCookie()
+    cookie.load(query)
+    cookies = {k: v.value for k, v in cookie.items()}
+    return cookies
 
 
 if __name__ == '__main__':
@@ -19,3 +25,16 @@ if __name__ == '__main__':
     assert parse_cookie('') == {}
     assert parse_cookie('name=Dima;age=28;') == {'name': 'Dima', 'age': '28'}
     assert parse_cookie('name=Dima=User;age=28;') == {'name': 'Dima=User', 'age': '28'}
+    assert parse_cookie('{ key: value}') == {}
+    assert parse_cookie('color=Grey;name=Silver;') == {'color': 'Grey', 'name': 'Silver'}
+    assert parse_cookie('name=Sam=Admin;status=False;') == {'name': 'Sam=Admin', 'status': 'False'}
+    assert parse_cookie('[]') == {}
+    assert parse_cookie('name=Valera;age=28;;!;') == {'name': 'Valera', 'age': '28'}
+    assert parse_cookie('name=Dima=User;age=28=55;') == {'name': 'Dima=User', 'age': '28=55'}
+    assert parse_cookie('//1 }{ \\//') == {}
+    assert parse_cookie('name=Not_found;status=unknown;') == {'name': 'Not_found', 'status': 'unknown'}
+    assert parse_cookie('123421=FFF=User;age=28;') == {'123421': 'FFF=User', 'age': '28'}
+    assert parse_cookie('name=Sarah;age=28;status=blocked;') == {'name': 'Sarah', 'age': '28', 'status': 'blocked'}
+    assert parse_cookie('SSSrSSS=!!!!=User=22565;age=28;') == {'SSSrSSS': '!!!!=User=22565', 'age': '28'}
+    assert parse_cookie('//1 }{ \\      //') == {}
+
